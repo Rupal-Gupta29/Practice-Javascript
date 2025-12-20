b6282146e33c288798d874da4a75c601f6fa6ab3
Rupal <rupalgupta951@gmail.com>
2025-12-21 02:31:34 +0530

    added reduce example

--- mapForEachFilter.js
+++ mapForEachFilter.js
@@ -19,3 +19,18 @@ names.forEach((name) => {
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 let isEven = numbers.filter((num) => num % 2 == 0);
 console.log(isEven);
+
+//reduce
+
+const emps = [
+  { name: "John", salary: 56000 },
+  { name: "David", salary: 78000 },
+  { name: "Jose", salary: 98000 },
+];
+
+const totalSalary = emps.reduce((total, emp) => {
+  total = total + emp.salary;
+  return total;
+}, 0);
+
+console.log(totalSalary);
