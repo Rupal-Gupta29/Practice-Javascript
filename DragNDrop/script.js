871675fd141ae3b325ff9f377581e5e321e78251
Rupal <rupalgupta951@gmail.com>
2026-01-08 17:03:00 +0530

    Added simple drag and drop

---
+++ DragNDrop/script.js
@@ -0,0 +1,22 @@
+let draggables = document.querySelectorAll(".draggable");
+let containers = document.querySelectorAll(".container");
+
+draggables.forEach((draggable) => {
+  draggable.addEventListener("dragstart", () => {
+    draggable.classList.add("dragging");
+  });
+});
+
+draggables.forEach((draggable) => {
+  draggable.addEventListener("dragend", () => {
+    draggable.classList.remove("dragging");
+  });
+});
+
+containers.forEach((container) => {
+  container.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    const draggable = document.querySelector(".dragging");
+    container.appendChild(draggable);
+  });
+});
